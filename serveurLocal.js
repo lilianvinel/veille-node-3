7fac32dc97d1e1ce4ec4fd5c0124522373a60d91
Lilian Vinel <e1565626@cmaisonneuve.qc.ca>
2018-02-05 15:25:35 -0500

    ajout de la fonction pour extraire les propriétés du fichier JSON

--- serveurLocal.js
+++ serveurLocal.js
@@ -1,11 +1,22 @@
-"use strict";
-/* importation du module «fs» (file system) */
-const fs = require("fs");
-const http = require("http"); 
-http.createServer((request, response) => 
-{ 
- console.log('branchement sur le port 3000')
- response.writeHead(200, {"Content-Type": "text/plain; charset=UTF-8"});
- response.write(fs.readFileSync('Provinces.json', 'utf8')); 
- response.end(); 
-}).listen(3000);
+'use strict';
+
+let fs = require('fs');
+let provincesJSON = JSON.parse(fs.readFileSync('Provinces.json'));
+/* 
+permet d'extraire l'ensemble des propriétés valeurs de l'objet litéral */  
+
+const http = require("http");
+let server = http.createServer((request, response) => {
+
+response.writeHead(200, {"Content-Type": "text/html; charset=UTF-8"});
+ response.write('<style>h1 {font-family:arial; font-size: 3em;}</style>')
+ response.write('<!DOCTYPE "html">');
+ response.write('<html>');
+ response.write('<head>');
+ response.write('</head>');
+ response.write('<body>');
+ response.write('</body>');
+ response.write('</html>');
+ response.end();
+
+}).listen(3000)
