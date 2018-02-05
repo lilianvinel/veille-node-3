0e573bade9b8a43bd9d66d4c4a4c167f97227fdc
Lilian Vinel <e1565626@cmaisonneuve.qc.ca>
2018-02-05 15:27:47 -0500

    création du HTML qui contiendra le tableau & création du tableau HTML à partir du JSON

--- serveurLocal.js
+++ serveurLocal.js
@@ -3,7 +3,18 @@
 let fs = require('fs');
 let provincesJSON = JSON.parse(fs.readFileSync('Provinces.json'));
 /* 
-permet d'extraire l'ensemble des propriétés valeurs de l'objet litéral */  
+permet d'extraire l'ensemble des propriétés valeurs de l'objet litéral */
+
+const contenu_objet_json=(o)=>{
+   let trace = '<table><tr><th><h3>Acronymes</h3></th><th><h3>Provinces</h3></th></tr>';
+   for (let p in o) { 
+     trace +='<tr><td>'+ p + '</td><td>' + o[p] + '</td></tr>'; 
+   } 
+   trace += '</table>'
+   return trace;
+   }
+
+//serveur   
 
 const http = require("http");
 let server = http.createServer((request, response) => {
@@ -15,6 +26,7 @@ response.writeHead(200, {"Content-Type": "text/html; charset=UTF-8"});
  response.write('<head>');
  response.write('</head>');
  response.write('<body>');
+ response.write(contenu_objet_json(provincesJSON));
  response.write('</body>');
  response.write('</html>');
  response.end();
