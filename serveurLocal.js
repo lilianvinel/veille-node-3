815ac1adfefbedef8671963f65f6f076cb3db07c
Lilian Vinel <e1565626@cmaisonneuve.qc.ca>
2018-02-05 11:52:23 -0500

    lecture du fichier JSON en UTF-8

--- serveurLocal.js
+++ serveurLocal.js
@@ -1,8 +1,11 @@
+"use strict";
+/* importation du module «fs» (file system) */
+const fs = require("fs");
 const http = require("http"); 
 http.createServer((request, response) => 
 { 
- console.log('branchement sur le port 8888')
- response.writeHead(200, {"Content-Type": "text/plain"});
- response.write("Hello World"); 
+ console.log('branchement sur le port 3000')
+ response.writeHead(200, {"Content-Type": "text/plain; charset=UTF-8"});
+ response.write(fs.readFileSync('Provinces.json', 'utf8')); 
  response.end(); 
-}).listen(8888);
+}).listen(3000);
